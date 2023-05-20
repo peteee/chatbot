8d81e3b7064dd0c6cad456bf9163478f7471cb01
Peter Oberlaender <pete@wayoutofline.net>
2023-05-20 10:08:05 -0400

    improvements...

--- js/chatbot.js
+++ js/chatbot.js
@@ -19,6 +19,15 @@ let micBtn = document.getElementById("record");
 const myAnswer = document.getElementById("answer");
 const myQuestionBox = document.getElementById("command");
 
+// Robot elements
+let robotMouth = document.getElementById("mouth");
+
+// Time function
+// we add an additional 0 for numbers smaller than 10 
+function checkTime(i) {
+    return (i < 10) ? "0" + i : i;
+}
+
 micBtn.onclick = function() {
     recognition.start();
     console.log('Ready to receive a command.');
@@ -26,17 +35,44 @@ micBtn.onclick = function() {
 
 function patternTest(pattern, command) {
     let result = pattern.test(command.toLowerCase());
-    weHaveAMatch = (result) ? true : false;
+    //weHaveAMatch = (result) ? true : false;
     return result;
+}
+
+function animateMouth(sentence) {
+    robotMouth.classList.add("speak");
+    let wordCount = sentence.split(" ").length;
+    let speakTimer = wordCount * 500; //each word lasts half a second (=500ms)
+    setTimeout(function(){
+        //do stuff
+        robotMouth.classList.remove("speak");
+    }, speakTimer);
 }
 
 function parseCommand(command) {
     console.log(command);
     myQuestionBox.value = "";
 
+    if(patternTest(/the time/g, command) || patternTest(/what time/g, command)) {
+        let today = new Date(),
+            h = today.getHours(),
+            m = today.getMinutes()//,
+            //s = checkTime(today.getSeconds());
+
+        //let timeToRead =  "It is now "+ h + " o'clock, " + m + " minutes and " + s + " seconds";
+        let timeToRead =  "It is now "+ h + " o'clock and " + m + " minutes";
+
+        myAnswer.innerHTML = timeToRead;
+        let utterance0 = new SpeechSynthesisUtterance(timeToRead);
+        speechSynthesis.speak(utterance0);
+        weHaveAMatch = true;
+        animateMouth(timeToRead);
+    } 
     if(patternTest(/i like pizza/g, command)) {
         console.log("Yay! Yummy :P");
         hints.innerHTML = '<img src="https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.supreme-lovers.3706cdc20b0752ac212c0d68a310fb18.1.jpg">'
+        weHaveAMatch = true;
+        animateMouth("Yay! Yummy :P");
     }
 
     // Look for "i like pizza"
@@ -52,22 +88,28 @@ function parseCommand(command) {
         hints.innerHTML = '<img src="https://stordfkenticomedia.blob.core.windows.net/df-us/rms/media/recipemediafiles/recipes/retail/x17/rainbow-cake760x580.jpg?ext=.jpg">';
         let utterance = new SpeechSynthesisUtterance("Yummy cake");
         speechSynthesis.speak(utterance);
+        weHaveAMatch = true;
+        animateMouth("Yummy cake");
     }
 
     if(patternTest(/vegas/g, command)) {
         top.location.href = "https://www.expedia.ca/Cheap-Flights-To-Las-Vegas.d178276.Travel-Guide-Flights"
         let utterance2 = new SpeechSynthesisUtterance("Have a safe trip");
         speechSynthesis.speak(utterance2);
+        weHaveAMatch = true;
+        animateMouth("Have a safe trip");
     }
 
     if(patternTest(/my favourite show on youtube/g, command)) {
         console.log("VIDEO")
         hints.innerHTML = '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/EorJ8cEzsZo?start=1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+        weHaveAMatch = true;
     }
 
     if(patternTest(/i like red/g, command)) {
         let myBG = document.querySelector("body");
         myBG.style.backgroundColor = "red";
+        weHaveAMatch = true;
     }
 
     (async function callJSONData() {
@@ -93,24 +135,29 @@ function parseCommand(command) {
                 let utterance = new SpeechSynthesisUtterance(currentAnswer);
                 speechSynthesis.speak(utterance);
                 myAnswer.innerHTML = currentAnswer;
+                weHaveAMatch = true;
+                animateMouth(currentAnswer);
             }
             // if(item.question === command) {
             //     alert("success");
             //     alert(item.answer);
             // }
-
-
-
         });
-        // if(!weHaveAMatch) {
-        //     currentAnswer = "Sorry, I don't know...";
-        //     let utterance = new SpeechSynthesisUtterance(currentAnswer);
-        //     speechSynthesis.speak(utterance);
-        //     myAnswer.innerHTML = currentAnswer;
-        // }
-        // weHaveAMatch = false;
+
+        ////If we don't have a match
+        if(!weHaveAMatch) {
+            currentAnswer = "Sorry, what is " + command;
+            let utterance = new SpeechSynthesisUtterance(currentAnswer);
+            speechSynthesis.speak(utterance);
+            myAnswer.innerHTML = currentAnswer;
+            animateMouth(currentAnswer);
+        }
+        // reset match variable
+        weHaveAMatch = false;
+
     })();
     // callJSONData();
+
 
 }
 
